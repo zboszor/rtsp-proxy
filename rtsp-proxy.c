ffbfc4c8cdbcc948d61a7bdeac85ebe226763681
Zoltán Böszörményi <zboszor@gmail.com>
2025-02-28 09:04:29 +0100

    Handle short option -i
    The short option alias of --ini was accidentally left out.
    
    Signed-off-by: Zoltán Böszörményi <zboszor@gmail.com>

--- rtsp-proxy.c
+++ rtsp-proxy.c
@@ -516,7 +516,7 @@ int main(int argc, char **argv) {
 	signal(SIGTERM, sighandler);
 
 	while (1) {
-		int c = getopt_long(argc, argv, "hs:d:r:f:w:", opts, NULL);
+		int c = getopt_long(argc, argv, "his:d:r:f:w:", opts, NULL);
 
 		if (c == -1)
 			break;
